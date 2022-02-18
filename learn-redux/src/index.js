81e96e13368b8479dfc1d2e150900897295ca737
dlstn4509 <dlstn7609@hanmail.net>
2022-02-18 10:22:58 +0900

    redux

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { rootReducer } from './modules';
+
+const store = createStore(rootReducer, composeWithDevTools()); // 스토어를 만듭니다.
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ learn-redux/src/modules/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import { counter } from './counter';
+import { todos } from './todos';
+
+export const rootReducer = combineReducers({
+  counter,
+  todos,
+});
